cf9600c46520aea371e1f4b0d7a01b4a178c4607
Alex Brown <alex@robertwbrown.net>
2017-11-05 21:25:12 -0500

    Adding api call to urban dict

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
@@ -11,7 +12,23 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+function parseResultsAndGetDefinition(res){
+  let response = JSON.parse(res);
+  let def;
+  if(response.result_type != "no_results"){
+    def = response.list[0].definition 
+  } else {
+    def = "We couldn't find any definitions for that word."
+  }
+  return def;    
+}
+
 app.post('/lookup', function(req, res){
+  request('http://api.urbandictionary.com/v0/define?term=banana', function(error, response, body){
+    let def = parseResultsAndGetDefinition(response.body);
+    console.log(def);
+  });
+
   var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.word ? req.body.result.parameters.word : "Could you say that again?"
   return res.json({
     speech:speech,
